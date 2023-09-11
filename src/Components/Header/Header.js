50c60179eef9c4c913383c1a998cb9568187044f
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2023-09-11 12:53:50 +0530

    added header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import headerStyle from  './Header.module.css'
+
+const Header = () => {
+
+
+    return <header className={headerStyle.header}>
+                <nav className={headerStyle.navbar}>
+                    <h6 className={headerStyle.heading}> 
+                        <a href='/'>tenor</a>
+                    </h6>
+                    <div>
+                        <a href='/' className={headerStyle.createBtn}>Create</a>
+                        <a href='/' className={headerStyle.signInBtn}>Sign In</a>
+                    </div>
+                </nav>
+
+           </header>
+
+};
+
+
+export default Header;
