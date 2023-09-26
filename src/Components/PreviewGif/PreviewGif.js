4692633b94387bebd754b1ed020cda5ec45e432a
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2023-09-27 01:20:39 +0530

    implemented share url

--- src/Components/PreviewGif/PreviewGif.js
+++ src/Components/PreviewGif/PreviewGif.js
@@ -1,14 +1,43 @@
+import React, { useState } from 'react';
 import styles from'./PreviewGif.module.css';
 
 
 const PreviewGif = (props) => {
 
+    const [isCopying, setIsCopying] = useState(false)
+
+    const copyUrlHandler = async () => {
+
+        try {
+            setIsCopying(true);
+            await navigator.clipboard.writeText(props.gif.gifUrl);
+            setTimeout(()=> {
+                setIsCopying(false);
+            }, 200);
+        } catch(e) {
+            console.log(e);
+        }
+    }
 
 
-    return <div className={styles.previewGif}>
-                <h2>{props.gif.desc}</h2>
-                <img src={props.gif.gifUrl} alt={props.gif.desc}/>
-            </div> 
+
+    return <React.Fragment>
+                <div className={styles.previewGif}>
+                    <h2>{props.gif.desc}</h2>
+                    <img src={props.gif.gifUrl} alt={props.gif.desc}/>
+                </div> 
+                <div className={styles.url}>
+                    <h3>
+                        Share URL
+                    </h3>
+                    <div className={styles.urlContainer} onClick={copyUrlHandler}>
+                        {props.gif.gifUrl}
+                    { isCopying && <div className={styles.status}>Copied Successfully!</div>}
+                    </div>
+                </div>
+            </React.Fragment>
+    
+    
 
 
 }
